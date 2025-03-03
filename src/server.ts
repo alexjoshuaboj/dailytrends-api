470dca911a6d6b4e18babefe8b8595cb6efa395f
Alex Joshua <61251126+alexjoshuaboj@users.noreply.github.com>
2025-03-03 21:29:31 +0100

    feat: server config and helmet security

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import routes from '@routes/index';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
 
+dotenv.config();
 export class Server {
     private app: express.Application;
     private port: string;
@@ -7,6 +11,34 @@ export class Server {
     constructor(port: string) {
         this.port = port;
         this.app = express();
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use((_, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            next();
+        });
+
+        this.app.use(helmet.xssFilter());
+        this.app.use(helmet.frameguard());
+        this.app.use(helmet.hidePoweredBy());
+        this.app.use(helmet.ieNoOpen());
+        this.app.use(helmet.noSniff());
+        this.app.use(helmet.dnsPrefetchControl());
+
+        this.app.get('/isAlive', (_, res) => {
+            res.send({
+                message: 'Server is alive'
+            });
+        });
+
+        this.app.use('/api', routes);
+
+        this.app.use((_, res) => {
+            res.status(404).send({
+                error: 'Not found'
+            });
+        });
     }
 
     async listen(): Promise<void> {
